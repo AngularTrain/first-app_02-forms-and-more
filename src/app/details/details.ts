2fd4edcc8437de232e37ec9da63ba5f073493ab9
simonwoodworth <s.woodworth@ucc.ie>
2026-01-01 16:39:41 +0000

    Slightly cleaner handling of undefined housing locations

--- src/app/details/details.ts
+++ src/app/details/details.ts
@@ -9,22 +9,23 @@ import {FormControl, FormGroup, ReactiveFormsModule} from '@angular/forms';
   imports: [ReactiveFormsModule],
   template: `
     <article>
+      @if (housingLocation) {
       <img
         class="listing-photo"
-        [src]="housingLocation?.photo"
-        alt="Exterior photo of {{ housingLocation?.name }}"
+        [src]="housingLocation.photo"
+        alt="Exterior photo of {{ housingLocation.name }}"
         crossorigin
       />
       <section class="listing-description">
-        <h2 class="listing-heading">{{ housingLocation?.name }}</h2>
-        <p class="listing-location">{{ housingLocation?.city }}, {{ housingLocation?.state }}</p>
+        <h2 class="listing-heading">{{ housingLocation.name }}</h2>
+        <p class="listing-location">{{ housingLocation.city }}, {{ housingLocation.state }}</p>
       </section>
       <section class="listing-features">
         <h2 class="section-heading">About this housing location</h2>
         <ul>
-          <li>Units available: {{ housingLocation?.availableUnits }}</li>
-          <li>Does this location have wifi: {{ housingLocation?.wifi }}</li>
-          <li>Does this location have laundry: {{ housingLocation?.laundry }}</li>
+          <li>Units available: {{ housingLocation.availableUnits }}</li>
+          <li>Does this location have wifi: {{ housingLocation.wifi }}</li>
+          <li>Does this location have laundry: {{ housingLocation.laundry }}</li>
         </ul>
       </section>
       <section class="listing-apply">
@@ -39,6 +40,9 @@ import {FormControl, FormGroup, ReactiveFormsModule} from '@angular/forms';
           <button type="submit" class="primary">Apply now</button>
         </form>
       </section>
+      } @else {
+      <p>Listing not found.</p>
+      }
     </article>
   `,
   styleUrls: ['./details.css'],
@@ -46,7 +50,7 @@ import {FormControl, FormGroup, ReactiveFormsModule} from '@angular/forms';
 export class Details {
   route: ActivatedRoute = inject(ActivatedRoute);
   housingService = inject(HousingService);
-  housingLocation: HousingLocationInfo | undefined;
+  housingLocation?: HousingLocationInfo;
   applyForm = new FormGroup({
     firstName: new FormControl(''),
     lastName: new FormControl(''),
--- src/app/housing.ts
+++ src/app/housing.ts
@@ -17,7 +17,7 @@ export class HousingService {
     async getHousingLocationById(id: number): Promise<HousingLocationInfo | undefined> {
       const data = await fetch(`${this.url}?id=${id}`);
       const locationJson = await data.json();
-      return locationJson[0] ?? {};
+      return locationJson[0] as HousingLocationInfo | undefined;
     }
 
     submitApplication(firstName: string, lastName: string, email: string) {
